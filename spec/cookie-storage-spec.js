e7d5357fdef5f569dae677e1eaecec465f0cabca
Chris Hicks <chrhicks@gmail.com>
2011-10-15 09:03:56 -0400

    ensuring testing starts with a document.cookie size of 0

--- spec/cookie-storage-spec.js
+++ spec/cookie-storage-spec.js
@@ -1,7 +1,11 @@
 describe('cookie-storage', function() {
   var cs = window.CookieStorage;
 
-  it('should get same data that was put', function() {
+  it('should have a cookie size of 0', function() {
+    expect(document.cookie.length).toEqual(0);
+  });
+
+  it('should "get" same data that was "put"', function() {
     cs.put('key', 'value');
     expect(cs.get('key')).toEqual('value');
   });
