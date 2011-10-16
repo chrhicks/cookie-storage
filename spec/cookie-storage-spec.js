db46b040006df4480823d37575f83370824bd0d9
Chris Hicks <chrhicks@gmail.com>
2011-10-15 22:40:23 -0400

    adding jasmine tests for cookie stores

--- spec/cookie-storage-spec.js
+++ spec/cookie-storage-spec.js
@@ -1,9 +1,11 @@
 describe('cookie-storage', function() {
   var cs = window.CookieStorage,
     cm = cs.CookieManager,
-    clearCookies;
+    clearCookies,
+    getRandomArbitary,
+    randomWords;
 
-  clearCookies = function() {
+  clearCookies = function () {
     _.each(document.cookie.split(';'), function (cookie) {
       var name = cookie.split('='),
         oExpDate = new Date();
@@ -13,19 +15,42 @@ describe('cookie-storage', function() {
     });
   };
 
-  beforeEach(function() {
+  getRandomArbitary = function (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  randomWords = function (count) {
+    var letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      minSize = 3,
+      maxSize = 16,
+      result = '';
+
+      _(count).times(function (n) {
+        _(getRandomArbitary(minSize, maxSize)).times(function () {
+          result += letters[getRandomArbitary(0, letters.length)];
+        });
+
+        if (n < count - 1) {
+          result += ' ';
+        }
+      });
+
+      return result;
+  };
+
+  beforeEach(function () {
     clearCookies();
   });
 
   /*
    * CookieStorage.CookieManger
    */
-  describe('cookie-manager', function() {
-    it('should return null', function() {
+  describe('cookie-manager', function () {
+    it('should return null', function () {
       expect(cm.get('does_not_exist')).toEqual(null);
     });
 
-    it('should get what was just set', function() {
+    it('should get what was just set', function () {
       var varName = 'shouldEquals',
         varValue = 'a test string ? ; = ,';
       cm.set(varName, cm.encode(varValue));
@@ -33,7 +58,7 @@ describe('cookie-storage', function() {
       expect(cm.get(varName)).toEqual(varValue);
     });
 
-    it('should remove the cookie', function() {
+    it('should remove the cookie', function () {
       var name = "cookieName",
         value = "value";
 
@@ -44,7 +69,7 @@ describe('cookie-storage', function() {
       expect(cm.get(name)).toEqual(null);
     });
 
-    it('should pass when set, fail when not set', function() {
+    it('should pass when set, fail when not set', function () {
       var name = "cookieName",
         value = "value";
 
@@ -59,7 +84,7 @@ describe('cookie-storage', function() {
   /*
    * CookieStorage
    */
-  it('should have a cookie size of 0', function() {
+  it('should have a cookie size of 0', function () {
     expect(document.cookie.length).toEqual(0);
   });
 
@@ -71,10 +96,37 @@ describe('cookie-storage', function() {
     expect(cs.get(name)).toEqual(value);
   });
 
-  it('should have an expected length', function() {
+  it('should have an expected length', function () {
     var name = "name",
       value = "her royal majesty queen elizabeth ii";
     cs.put(name, value);
     expect(cs.entrySize('name')).toEqual(51);
+  });
+
+  it('should have a no cookie stores', function () {
+    cs.clearStores();
+    cs.init();
+    expect(cs.listStores().length).toEqual(0);
+  });
+
+  it('should have a single cookie store', function () {
+    var name = "cookieName",
+      value = "value";
+    cs.init();
+    expect(cs.listStores().length).toEqual(0);
+    cs.put(name, value);
+    expect(cs.listStores().length).toEqual(1);
+  });
+
+  it('should span multiple cookie stores', function () {
+    var numEntries = 60;
+
+    cs.init();
+
+    _(numEntries).times(function () {
+      cs.put(randomWords(1), randomWords(getRandomArbitary(1, 11)));
+    });
+
+    expect(cs.listStores().length > 1).toEqual(true);
   });
 });
